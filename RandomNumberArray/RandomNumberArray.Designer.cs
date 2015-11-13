ede190452b0626b05c2e28c0cebf953cabf15384
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-13 13:23:10 -0500

    Array

---
+++ RandomNumberArray/RandomNumberArray.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RandomNumberArray
+{
+    partial class frmRandomNumberArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(102, 201);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(22, 41);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(110, 16);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "The number was:";
+            // 
+            // frmRandomNumberArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnClick);
+            this.Name = "frmRandomNumberArray";
+            this.Text = "Random Number Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Label lblNumber;
+    }
+}
+
---
+++ RandomNumberArray/RandomNumberArray.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by: Alex Mathias
+ * Created on: 13-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 5-02
+ * This program checks if the number was found or not.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumberArray
+{
+    public partial class frmRandomNumberArray : Form
+    {
+        public bool ArrayCheck(int[] array, int chosenNumber)
+        {
+            bool theValueExists = false;
+
+            foreach (int number in array)
+            {
+                if (number == chosenNumber)
+                {
+                    theValueExists = true;
+                }
+            }
+            return theValueExists;
+
+        }
+        public frmRandomNumberArray()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            //Variables
+            bool theValueExists;
+            int[] numbers = new int[5];
+            Random rnd = new Random();
+
+            //Process
+            for (int counter = 0; counter < 5; counter++)
+            {
+                numbers[counter] = rnd.Next(1, 10 + 1);
+            }
+
+            theValueExists = ArrayCheck(numbers, 4);
+
+            if(theValueExists == true)
+            {
+                this.lblNumber.Text = "The number was found";
+            }
+            else
+            {
+                this.lblNumber.Text = "The number was not found";
+            }
+        }
+    }
+}
